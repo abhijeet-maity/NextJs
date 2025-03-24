64a5aa4aef0d01b6c652dd2cddcf167eaef287ee
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-24 17:36:27 +0530

    added to handle route handlers

---
+++ src/comments/route.ts
@@ -0,0 +1,7 @@
+import { comments } from "./data";
+
+export async function GET() {
+    return new Response(JSON.stringify(comments), {
+        headers: { "Content-Type": "application/json" },
+    });
+}
