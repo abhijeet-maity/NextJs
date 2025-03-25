24ee10a17e0a2a5ff8feee83821fcb7758b6cfd9
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-25 17:42:46 +0530

    added API requests in app router.

--- src/comments/route.ts
+++
@@ -1,7 +0,0 @@
-import { comments } from "./data";
-
-export async function GET() {
-    return new Response(JSON.stringify(comments), {
-        headers: { "Content-Type": "application/json" },
-    });
-}
